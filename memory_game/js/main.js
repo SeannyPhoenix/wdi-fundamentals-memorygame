06c91c85f90d8dda500469d3a212af5ac814e2f1
Seanny Phoenix <seannyphoenix@gmail.com>
2020-02-08 12:12:52 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Welcome to Memory Game!");
+
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
