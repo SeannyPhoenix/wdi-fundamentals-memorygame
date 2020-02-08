dbc6463672e37a6d7c63bdb1ae3064fc8765975f
Seanny Phoenix <seannyphoenix@gmail.com>
2020-02-08 13:17:56 -0800

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,20 @@
-console.log("Welcome to Memory Game!");
+let cards = ['queen', 'queen', 'king', 'king'];
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("Player flipped " + cardsInPlay[0]);
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("Player flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
